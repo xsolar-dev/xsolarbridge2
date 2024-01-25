dc44395412cc151a4dc0d206b4c36669bcb5ff52
longdh <longdh@vng.com.vn>
2024-01-25 09:53:50 +0700

    update

--- src/CMqttBridge.cpp
+++ src/CMqttBridge.cpp
@@ -9,57 +9,92 @@
  * 
  */
 #include "CMqttBridge.h"
-#include "Poco/Task.h"
 #include "Poco/Util/ServerApplication.h"
+#include "Poco/Task.h"
+#include "Poco/Format.h"
 #include <mosquitto.h>
 #include <string>
 
 using Poco::Util::Application;
 using Poco::Util::ServerApplication;
 
+
+#define LOG_ERROR(str) { \
+    Application& app = Application::instance(); \
+    Application::instance().logger().error(str); \
+}
+
+#define LOG_INFO(str) { \
+    Application& app = Application::instance(); \
+    Application::instance().logger().information(str); \
+}
+
+#define LOG_CRIT(str) { \
+    Application& app = Application::instance(); \
+    Application::instance().logger().critical(str); \
+}
+
+
 CMqttBridge::CMqttBridge(const std::string& bridgeName, const CBridgeConfig& config)
     : Poco::Task(bridgeName), _config(config)
 {
-
 }
 
 CMqttBridge::~CMqttBridge()
 {
-
 }
 
+/**
+ * @brief Sink Disconnect callback
+ * 
+ * @param mosq 
+ * @param obj 
+ * @param rc 
+ */
 static void onDisconnect(struct mosquitto *mosq, void *obj, int rc) 
 {
     if (rc == MOSQ_ERR_SUCCESS) 
     {
+        // do nothing
+        LOG_INFO("Sink Disconnected");
     } 
     else 
     {
-        mosquitto_reconnect(mosq);
+        // reconnect client
+        LOG_INFO("Sink Connection Error, trying reconnected");
+        mosquitto_reconnect_async(mosq);
     }
 }
 
-// Callback function for handling incoming messages
+/**
+ * @brief source message callback
+ * 
+ * @param mosq 
+ * @param userdata 
+ * @param message 
+ */
 static void messageCallback(struct mosquitto* mosq, void* userdata, const struct mosquitto_message* message) 
 {
     struct mosquitto* mosqSink = (struct mosquitto*) userdata;
 
     if(message->payloadlen) 
     {
-        std::cout << "Received message on topic " << message->topic << ": " << (char*)message->payload << std::endl;
+        LOG_INFO(Poco::format("Received message on topic %s, topic: %s",message->topic, (char*)message->payload));
 
         // Publish a message
-        if (mosquitto_publish(mosqSink, nullptr, message->topic, message->payloadlen, message->payload, message->qos, false)) 
+        if (int rc = mosquitto_publish(mosqSink, nullptr, message->topic, message->payloadlen, message->payload, message->qos, false)) 
         {
-            std::cerr << "Error: Unable to publish the message." << std::endl;
+            LOG_ERROR(Poco::format("Error %d: Unable to publish the message.", rc));
+
+            if (rc == MOSQ_ERR_NO_CONN)
+                mosquitto_reconnect_async(mosqSink);
         }
     }     
 }
 
 void CMqttBridge::runTask()
 {
-    Application& app = Application::instance();
-    Application::instance().logger().information("Start working task ...");
+    LOG_INFO("Start working task ...");
 
     // Initialize the library
     mosquitto_lib_init();
@@ -68,7 +103,7 @@ void CMqttBridge::runTask()
     struct mosquitto* mosqSink = mosquitto_new(nullptr, true, (void*) this);
     if (!mosqSink) 
     {
-        std::cerr << "Error: Unable to create Mosquitto instance." << std::endl;
+        LOG_CRIT("Error: Unable to create Mosquitto instance.");
         return;
     }
     mosquitto_username_pw_set(mosqSink, this->_config._sink._userName.c_str(), this->_config._sink._password.c_str());
@@ -81,7 +116,7 @@ void CMqttBridge::runTask()
     struct mosquitto* mosqSource = mosquitto_new(nullptr, true, (void*) mosqSink);
     if (!mosqSource) 
     {
-        std::cerr << "Error: Unable to create Mosquitto instance." << std::endl;
+        LOG_CRIT("Error: Unable to create Mosquitto instance.");
         return;
     }
 
@@ -94,7 +129,8 @@ void CMqttBridge::runTask()
 
     if (mosquitto_connect(mosqSink, this->_config._sink._host.c_str(), _config._sink._port, keepAlive)) 
     {
-        std::cerr << "Error: Unable to connect to the sink broker." << std::endl;
+        LOG_CRIT("Error: Unable to connect to the sink broker.");
+
         mosquitto_destroy(mosqSink);
         mosquitto_lib_cleanup();
 
@@ -103,7 +139,8 @@ void CMqttBridge::runTask()
 
     if (mosquitto_connect(mosqSource, this->_config._source._host.c_str(), _config._source._port, keepAlive)) 
     {
-        std::cerr << "Error: Unable to connect to the source broker." << std::endl;
+        LOG_CRIT("Error: Unable to connect to the source broker.");
+
         mosquitto_destroy(mosqSource);
         mosquitto_lib_cleanup();
 
@@ -116,7 +153,8 @@ void CMqttBridge::runTask()
 
     if (mosquitto_subscribe(mosqSource, nullptr, topic, qos)) 
     {
-        std::cerr << "Error: Unable to subscribe to the topic." << std::endl;
+        LOG_CRIT("Error: Unable to subscribe to the topic.");
+
         mosquitto_destroy(mosqSource);
         mosquitto_lib_cleanup();
         
